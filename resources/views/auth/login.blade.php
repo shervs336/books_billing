6aaa01b5cc34c8ede7730d4af3ac3103b0e2166c
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2018-04-22 17:25:40 +0800

    remove links fix title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,7 +35,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="{{ url('/home') }}"><b>InfyOm </b>Generator</a>
+        <a href="{{ url('/home') }}"><b>Books </b>Billing</a>
     </div>
 
     <!-- /.login-logo -->
@@ -81,8 +81,7 @@
             </div>
         </form>
 
-        <a href="{{ url('/password/reset') }}">I forgot my password</a><br>
-        <a href="{{ url('/register') }}" class="text-center">Register a new membership</a>
+      
 
     </div>
     <!-- /.login-box-body -->
